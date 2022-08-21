b10c089564fc6c13de1c4447a71116e87184c4db
Ibodullo1994 <111687197+Ibodullo1994@users.noreply.github.com>
2022-08-21 20:41:02 +0500

    ygjhjh
    fvjubjkvbj

---
+++ javascript/ish.js
@@ -0,0 +1,42 @@
+const wordList = [
+    {
+        word: "amerika",
+        hint: "Where is Joe Biden president?"
+    },
+    {
+        word: "russia",
+        hint: "Where is Vladimir Putin the president?"
+    },
+    {
+        word: "china",
+        hint: "Where is Xi Jinping the president?ка 3"
+    },
+    {
+        word: "uzbekistan",
+        hint: "Where is Samarkand located?"
+    },
+    {
+        word: "england",
+        hint: "Where is London the capital?"
+    },
+    {
+        word: "brazil",
+        hint: "where is the foodball factory?"
+    },
+    {
+        word: "portugal",
+        hint: "In which country was Cristiano Ronaldo born?"
+    },
+    {
+        word: "argentina",
+        hint: "Where is Lionel Messi from?"
+    },
+    {
+        word: "brazil",
+        hint: "Where is Ronaldinho Gaucho from?"
+    },
+    {
+        word: "italy",
+        hint: "Where is the colosseum"
+    },
+]
